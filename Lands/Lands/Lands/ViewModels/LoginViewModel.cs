33da5efd022da0dd12ceb0ec006460014bbb4ffa
EreGitHub <rtayler561@gmail.com>
2020-12-02 13:25:38 -0400

    LogicaLoogin BaseViewModel

---
+++ Lands/Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,78 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Lands.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        #region Atributes       
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+        #region Properties
+        public string Email { get; set; }
+        public string Password
+        {
+            get { return password; }
+            set { SetValue(ref password, value); }
+        }
+        public bool IsRunning
+        {
+            get { return isRunning; }
+            set { SetValue(ref isRunning, value); }
+        }
+        public bool IsRememberd { get; set; }
+        public bool IsEnabled
+        {
+            get { return isEnabled; }
+            set { SetValue(ref isEnabled, value); }
+        }
+        #endregion
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "You must enter email", "accept");
+                return;
+            }
+            IsRunning = true;
+            IsEnabled = false;
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "You must enter password", "accept");
+                Password = string.Empty;
+                return;
+            }
+            if (Email != "1" && Password != "1")
+            {
+                IsRunning = false;
+                IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert("Error", "Email or Password incorrect", "accept");
+                return;
+            }
+            IsRunning = false;
+            IsEnabled = true;
+            await Application.Current.MainPage.DisplayAlert("Ok", "Fuck yeahh!!!", "accept");
+            return;
+        }
+        #endregion
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRememberd = true;
+            isEnabled = true;
+        }
+        #endregion
+    }
+}
